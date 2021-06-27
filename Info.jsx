7b3ba22a31e20216cff802c04a4a3c00d6a7ee14
AMRUTHA33 <86529305+AMRUTHA33@users.noreply.github.com>
2021-06-27 16:25:27 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function info() {
+  return(
+    <div className="note">
+      <h1>Java script and React.js</h1>
+<p>A basic web development  js bootcamp</p>
+</div>
+  );
+  }
+export default info;
